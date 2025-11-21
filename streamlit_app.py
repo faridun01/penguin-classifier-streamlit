9789cc6d603671895ce9dd3a31c4d59e404c019e
Faridun Rahimov <114636633+faridun01@users.noreply.github.com>
2025-11-21 20:07:52 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 App Name')
 
 st.write('Hello world!')
+
+with st.expander("DATA"):
+  st.write('**OUR DATA**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
+
