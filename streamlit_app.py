3fe97fc4e346c8690c327f9991105e40396323c3
Faridun Rahimov <114636633+faridun01@users.noreply.github.com>
2025-11-21 20:09:21 +0500

    Add scatter chart visualization for penguin data

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,6 @@ with st.expander("DATA"):
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
 
+with st.expander("Visualization"):
+    st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+
